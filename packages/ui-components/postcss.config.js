6322809c77ada100066e0ca07d797dcfefbb27cf
Mohamed V J <muhammed8089@gmail.com>
2022-12-06 00:43:41 +0530

    Added tailwind to ui components

---
+++ packages/ui-components/postcss.config.js
@@ -0,0 +1,1 @@
+module.exports = require('@nuxtmf/tailwind-preset/postcss.config.cjs')
---
+++ packages/ui-components/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [
+    require('@nuxtmf/tailwind-preset'),
+  ],
+  content: ["./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+}
